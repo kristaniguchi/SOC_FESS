f3eb072539b1815fea7c5183ba4c94d56e46ae9c
kristaniguchi <kristinetq@sccwrp.org>
2021-06-16 13:39:42 -0700

    updated decisions

--- level2_alteration_summary_synthesismaps_finaldecisions.R
+++ level2_alteration_summary_synthesismaps_finaldecisions.R
@@ -31,7 +31,7 @@ library(rgeos)
 #ASCI/CSCI
 level2.dir <- "C:/Users/KristineT/SCCWRP/SOC WQIP - Flow Ecology Study - General/Tier2_analysis/"
 #fname <- paste0(level2.dir, "09_metric_suitability_tally_condensed.csv") #update to directory
-fname <- paste0(level2.dir, "09_metric_suitability_tally_condensed_aliso_oso_small_creeks.csv") #all lspc subbasins
+fname <- paste0(level2.dir, "09_metric_suitability_tally_scaled_all_sites_June2021_subset.csv") #all lspc subbasins including San Juan and Oso/small tribs, Aliso
 
 #read in suitability data 
 suit_data <- read.csv(fname)
@@ -66,8 +66,8 @@ basins <- st_read("data/subbasin_boundaries_forSCCWRP.shp", quiet = T)
 #reach polylines
 reaches <- st_read('data/reaches_forSCCWRP.shp', quiet = T)
 
-#model source LSPC or Wildermuth
-source <- read.csv("L:/San Juan WQIP_KTQ/Data/SpatialData/Model_Subbasins_Reaches/New_Subbasins_forSCCWRP_12062019/Subbasins_inmodel_source.csv")
+#model source LSPC or Wildermuth - do not need model source
+#source <- read.csv("L:/San Juan WQIP_KTQ/Data/SpatialData/Model_Subbasins_Reaches/New_Subbasins_forSCCWRP_12062019/Subbasins_inmodel_source.csv")
 
 
 ############################################################
@@ -77,7 +77,7 @@ source <- read.csv("L:/San Juan WQIP_KTQ/Data/SpatialData/Model_Subbasins_Reache
 basin_comid_lookup <- read.csv("L:/San Juan WQIP_KTQ/Data/SpatialData/v13_pourpoints_NHD_comids.csv")
 
 #lookuptable to convert subbasin codes for model output subbasin names
-subbasin_lookup <- read.csv("L:/San Juan WQIP_KTQ/Data/RawData/From_Geosyntec/South_OC_Flow_Ecology_for_SCCWRP/191220_Interim_Calibration/site_name_lookupletternumbers.csv")
+subbasin_lookup <- read.csv("L:/San Juan WQIP_KTQ/Data/RawData/From_Geosyntec/South_OC_Flow_Ecology_for_SCCWRP/Old_Runs/191220_Interim_Calibration/site_name_lookupletternumbers.csv")
 
 #convert basin orig name to outputfile name (model subbasin name)
 new.subbasinname <- basin_comid_lookup$Subbasin
@@ -102,13 +102,15 @@ suit_data2 <- suit_data %>%
 #####post process to get overall alteration
 
 ####################################
-#DECISIONS: Use 50% probabilty threshold
-suit_data2_50 <- suit_data2[suit_data2$Threshold == "Threshold50",]
+#DECISIONS: Use the probability threshold and bio threshold combo that provides the most discriminatory power from Katie's analysis
+  #OLD: Use 50% probabilty threshold
+  #suit_data2_50 <- suit_data2[suit_data2$Threshold == "Threshold50",]
+suit_data2_50 <- data.frame(suit_data2) #dont need to subset since it already contains the appropriate prob threshold, just use as is
 
 ####################################
-#if >75% time Altered, altered
-suit_data2_50$alteration_75pct_time[suit_data2_50$Altered > 75] <- "Altered"
-suit_data2_50$alteration_75pct_time[suit_data2_50$Altered <= 75] <- "Unaltered"
+#if >50% time Altered, altered
+suit_data2_50$alteration_75pct_time[suit_data2_50$Altered > 50] <- "Altered"
+suit_data2_50$alteration_75pct_time[suit_data2_50$Altered <= 50] <- "Unaltered"
 
 #### if 1 metric is altered, then all altered
 #aggregate site, Biol, Threshold, alteration_75pct_time
@@ -120,42 +122,18 @@ subset.75pct.time <- suit_data2_50 %>%
 #save as dataframe
 subset.75pct.time <- data.frame(subset.75pct.time)
 
-
-
 #2-3 metric altered, considered altered
 #if number altered > 1 --> class it as overall altered
 subset.75pct.time$overall.altered.2metric <- NA
 subset.75pct.time$overall.altered.2metric[which(subset.75pct.time$alteration_75pct_time == "Altered" & subset.75pct.time$n > 1)] <- "Altered"
-#if 3 metrics unaltered --> class it as unaltered
+#if 2-3 unaltered, consider unaltered                                                                                                                                                                                                                         #if 2-3 metrics unaltered --> class it as unaltered
 subset.75pct.time$overall.altered.2metric[which(subset.75pct.time$alteration_75pct_time == "Unaltered" & subset.75pct.time$n > 1)] <- "Unaltered"
 
-#########################
-
-###Do not process wildermuth for annual report ####
-#format wildermuth data and tally
-#subset.wildermuth <- suit_data_wildermuth %>% 
-  #group_by(New_Name, Biol, Threshold, alteration_75pct_time) %>% 
-  #tally() %>% 
-  #ungroup()
-#save as dataframe
-#subset.wildermuth <- data.frame(subset.wildermuth)
-#remove subbasins that are NA (Q99 rows)
-#subset.wildermuth <- subset.wildermuth[-which(is.na(subset.wildermuth$New_Name)),]
-
-#Wildermuth: 2 metric altered, considered altered (doesn't have Q99 so no 3rd metric)
-#if number altered > 1 --> class it as overall altered
-#subset.wildermuth$overall.altered.2metric <- NA
-#subset.wildermuth$overall.altered.2metric[which(subset.wildermuth$alteration_75pct_time == "Altered" & subset.wildermuth$n  == 2)] <- "Altered"
-#if 1 or 2 metrics unaltered --> class it as unaltered
-#subset.wildermuth$overall.altered.2metric[which(subset.wildermuth$alteration_75pct_time == "Unaltered" & subset.wildermuth$n > 0)] <- "Unaltered"
-#if NA stay NA
-#combine with lspc data
-#subset.75pct.time.all <- merge(subset.75pct.time, subset.wildermuth, all = TRUE)
 
 #########################
 
 #save csv
-file.name <- paste0(level2.dir, "summary.50prob.75pct.time.altered.2metrics_aliso_oso_small_creeks.csv")
+file.name <- paste0(level2.dir, "summary.50prob.75pct.time.altered.2metrics_LSPC_All_06102021.csv")
 write.csv(subset.75pct.time, file=file.name, row.names = FALSE)
 
 
@@ -185,20 +163,13 @@ data.frame(subset.75pct.time.summary2)
 #create synthesis of CSCI and ASCI alteration:
   #if both altered, high priority; if unaltered --> low priority
 
-#if using wildermuth:
-#remove NA values
-#subset.75pct.time.all2 <- na.omit(subset.75pct.time.all)
-#subset.75pct.time.summary2.all <- subset.75pct.time.all %>% 
-  #group_by(New_Name, Threshold, overall.altered.2metric) %>% 
-  #tally() %>% 
-  #ungroup() %>% 
-  #na.omit()  
-#overall.summary <- data.frame(subset.75pct.time.summary2.all)
 
 #remove NA values
 subset.75pct.time.all2 <- na.omit(subset.75pct.time)
-subset.75pct.time.summary2.all <- subset.75pct.time %>% 
-  group_by(New_Name, Threshold, overall.altered.2metric) %>% 
+#tally number of biol (csci/asci) altered per site
+subset.75pct.time.summary2.all <- subset.75pct.time.all2 %>% 
+  group_by(New_Name,  overall.altered.2metric) %>% 
+  #group_by(New_Name, Threshold, overall.altered.2metric) %>% 
   tally() %>% 
   ungroup() %>% 
   na.omit()  
@@ -222,7 +193,7 @@ synthesis.summary <- na.omit(overall.summary)
 #summary
 synthesis.summary.table <- synthesis.summary %>% 
   na.omit()  %>% 
-  group_by(Threshold, synthesis_alteration) %>% 
+  group_by(synthesis_alteration) %>% 
   tally() %>% 
   ungroup() %>% 
   mutate(pct.2metric = 100*n/32) 
@@ -276,43 +247,23 @@ source2 <- synthesis.summary %>%
     scale_fill_manual(name = "Priority based on Biologic Flow Alteration", labels = lookup.sub$categories, values=lookup.sub$colors) +
     geom_sf(data = reaches, color = "#67a9cf", size = 0.5) 
   
-  #add in model source
-  syn.plot2 <- syn.plot + geom_sf(data = source2, size = 1, fill = NA, aes(color=Source, geometry = geometry)) +
-    scale_color_manual(name = "Model Source", labels = c("LSPC", "GSFLOW"), values=c("black", "hotpink")) +
-    geom_sf(data = reaches, color = "#67a9cf", size = 0.5) 
-  
   #print
   print(syn.plot)
-  print(syn.plot2)
-  
+
 
   #write plot
-  out.filename <- paste0(out.dir, "Synthesis_Prioritization_map_Aliso_Oso_small_creeks.jpg")
+  out.filename <- paste0(out.dir, "Synthesis_Prioritization_map_LSPC_all_Aliso_Oso_small_creeks_SJ.jpg")
   ggsave(syn.plot, file = out.filename, dpi=500, height=6, width=8)
   
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 ############################################################
-#create suitability maps for 50% probabily, for 75% of time altered altered, 2 metrics
+#create suitability maps for appropriate prob and biol threshold combos, for 50% of time altered altered, 2 metrics
 
 #names
 #cols <- names(subset.75pct.time.all)
-cols <- names(subset.75pct.time)
+cols <- names(subset.75pct.time.all2)
 col.names <- "overall.altered.2metric"
 
 #colors for suitability categories
@@ -395,7 +346,7 @@ for(i in 7){
       print(syn.plot)
       
       #write plot
-      out.filename <- paste0(out.dir, z, "_alteration_map_Aliso_Oso_small_creeks.jpg")
+      out.filename <- paste0(out.dir, z, "_alteration_map_LSPC_Aliso_Oso_small_creeks_SJ.jpg")
       ggsave(syn.plot, file = out.filename, dpi=300, height=4, width=6)
       
     }
@@ -411,6 +362,7 @@ subset$`Alteration - Biology` <- gsub("Unaltered", "Likely Unaltered", subset$`A
 #save the subset summary table with indices, subbasin
 #pivot wider to get hydro.alteration.CSCI and hydro.alteration.ASCI columns
 subset2 <- subset %>% 
+  select(-c(Probability_Threshold)) %>% 
   pivot_wider(names_from = Biol, values_from = `Alteration - Biology`) %>% 
   rename(hydro.alteration.CSCI = CSCI) %>% 
   rename(hydro.alteration.ASCI = ASCI)
