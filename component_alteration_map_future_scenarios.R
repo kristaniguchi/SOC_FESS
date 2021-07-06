234440bf47a785b1bb34b9f27749d9171abc72a7
kristaniguchi <kristinetq@sccwrp.org>
2021-07-06 16:00:25 -0700

    future maps for level 1 and 2 based on alteration direction and future change direction

--- component_alteration_map_future_scenarios.R
+++ component_alteration_map_future_scenarios.R
@@ -1,4 +1,9 @@
-#Flow component alteration map for future scenario!
+#Flow component alteration map for future scenarios!
+  #Make change maps using percent change from hist to current and current alteration direction
+  #if it's likely altered high and percent change increases --> worse
+  #if it's likely altered low and percent change decreases --> worse
+  #if it's opposite to two conditions, then conditions could improve
+  #if it's likely unaltered --> could be uncertain
 
 #install.packages("ggsn")
 #install.packages("ggmap")
@@ -7,9 +12,9 @@
 #install.packages("rgeos")
 #to install spDataLarge
 #devtools::install_github("robinlovelace/geocompr")
-#library(spData)
+library(spData)
 #install.packages("spDataLarge", repos = "https://nowosad.github.io/drat/", type = "source")
-#library(spDataLarge)
+library(spDataLarge)
 
 library(tidyverse)
 library(readxl)
@@ -23,6 +28,321 @@ library(geosphere)
 library(rgeos)
 
 
+#####
+########
+
+#load in spatial data
+#subbasin polygon shapefile - updated shapefile with Bell Canyon subbasin added
+basins <- st_read("data/Agg_Boundaries_v14.shp", quiet = T)
+#reach polylines
+reaches <- st_read('data/reaches_forSCCWRP_Aliso.shp', quiet = T)
+
+#lookuptable to convert subbasin codes for model output subbasin names
+subbasin_lookup <- read.csv(file="L:/San Juan WQIP_KTQ/Data/SpatialData/SOC_FESS_Subbasin_NHD.COMID_Lookup_Table.csv") %>% 
+  select(Subbasin, new.subbasinname) %>% 
+  rename(New_Name = Subbasin,
+         subbasin.model = new.subbasinname)
+
+
+#read in the alteration direction CSCI/ASCI df
+csci.asci.alt.direction <- read.csv("C:/Users/KristineT.SCCWRP2K/SCCWRP/SOC WQIP - Flow Ecology Study - General/Tier2_analysis/09_altered_metrics_direction_current_June2021.csv") %>% 
+  rename(subbasin.model = site,
+         metric = Hydro_Metric) %>% 
+  left_join(subbasin_lookup, by = "subbasin.model")
+
+#alteration directory
+alt.dir.name <- "Future_Climate_Aliso_ALL_lowflowbias"
+alteration.dir <- paste0("L:/San Juan WQIP_KTQ/Data/RawData/From_Geosyntec/South_OC_Flow_Ecology_for_SCCWRP/KTQ_flowalteration_assessment/", alt.dir.name)
+
+#set directory for output plots
+dir.output.name <- "AlterationMaps"
+dir.output <- paste0(alteration.dir, "/", dir.output.name)
+dir.create(dir.output)
+#directory for level 1 plots
+dir.output.1 <- paste0(dir.output, "/Level1")
+dir.create(dir.output.1)
+#directory for level 2 plots
+dir.output.2 <- paste0(dir.output, "/Level2")
+dir.create(dir.output.2)
+
+
+#Load percent change historical to mid-century csv
+pct.change.data <- read.csv("L:/San Juan WQIP_KTQ/Data/RawData/From_Geosyntec/South_OC_Flow_Ecology_for_SCCWRP/KTQ_flowalteration_assessment/Future_Climate_Aliso_ALL_lowflowbias/pct.change.percentiles.FFM.future.climate.all.scenarios.Aliso.ALL.lowflowbias.csv")
+
+#filter to p50 pct change only
+#subbasin.model.p50 <- pct.change.data %>% 
+#  filter(percentile == "p50")
+
+#remove all peak mag metrics
+pct.change.data <- pct.change.data[-grep("Peak_10", pct.change.data$metric),]
+pct.change.data <- pct.change.data[-grep("Peak_2", pct.change.data$metric),]
+pct.change.data <- pct.change.data[-grep("Peak_5", pct.change.data$metric),]
+#remove peak timing metric
+pct.change.data <- pct.change.data[-grep("Peak_Tim", pct.change.data$metric),]
+
+
+#read in FFM alteration statuses under current conditions
+ffm.alteration.curr <- read.csv("L:/San Juan WQIP_KTQ/Data/RawData/From_Geosyntec/South_OC_Flow_Ecology_for_SCCWRP/KTQ_flowalteration_assessment/Aliso_Oso_SmCk_SanJuan_all_lowflowbias/ffm_alteration.df.overall.join.Aliso.Oso.SmallCreeks.SanJuanLSPC.lowflowbias.csv")
+#exclude peak mag metrics
+ffm.alteration.curr <- ffm.alteration.curr[ffm.alteration.curr$title_component != "Peak Flow Magnitude" , ]
+#subset to aliso watersheds only!
+#take first 3 digits of subbasin.model
+subbasin.digits3 <- as.numeric(substr(ffm.alteration.curr$subbasin.model, 1, 3))
+#subset to subbasins that start with 201-207 (Aliso only)
+ffm.alteration.curr <- ffm.alteration.curr[subbasin.digits3 >= 201 &  subbasin.digits3 <= 207, ]
+
+#Combine the pct change p50 to the ffm.alteration.curr
+ffm.alt.curr.join <- pct.change.data %>% 
+  left_join(ffm.alteration.curr, by = c("subbasin.model", "metric"))
+
+
+#for every subbasin.gcm.metric.percentile, if alteration direction is high and abs(pct.change < -0.1)
+
+ffm.alt.curr.join$future.category <- case_when(
+  #if alteration direction is high and percent change is less than -10%, possible improvement
+  ffm.alt.curr.join$alteration.direction == "high" & ffm.alt.curr.join$pct.change < -0.1 ~ "Possible Improvement",
+  #if alteration direction is low and percent change is more than 10%, possible improvement
+  ffm.alt.curr.join$alteration.direction == "low" & ffm.alt.curr.join$pct.change > 0.1 ~ "Possible Improvement",
+  
+  #if alteration direction is high and percent change is more than 10%, possible degradation
+  ffm.alt.curr.join$alteration.direction == "high" & ffm.alt.curr.join$pct.change > 0.1  ~ "Possible Degradation",
+  #if alteration direction is low and percent change is less than -10%, possible degradation
+  ffm.alt.curr.join$alteration.direction == "low" & ffm.alt.curr.join$pct.change < -0.1 ~ "Possible Degradation",
+  
+  #if alteration direction is high and percent change is less than or = 10% and >= -10%, uncertain
+  ffm.alt.curr.join$alteration.direction == "high" & ffm.alt.curr.join$pct.change <= 0.1 & ffm.alt.curr.join$pct.change >= -0.1 ~ "Uncertain",
+  #if alteration direction is low and percent change is less than or = 10% and >= -10%, uncertain
+  ffm.alt.curr.join$alteration.direction == "low" & ffm.alt.curr.join$pct.change <= 0.1 & ffm.alt.curr.join$pct.change >= -0.1 ~ "Uncertain",
+  
+  
+  #if alteration direction is none found (unaltered) then uncertain
+  ffm.alt.curr.join$alteration.direction == "none_found"   ~ "Uncertain",
+  #if alteration direction is NA, then NA
+  is.na(ffm.alt.curr.join$alteration.direction)  ~ "Uncertain",
+)
+
+################################################################
+#Loop to make maps for each FFM and GCM, percentile % change
+#create gcm.metric.percentile column
+ffm.alt.curr.join$gcm.metric.percentile <- paste(ffm.alt.curr.join$gcm, ffm.alt.curr.join$metric, ffm.alt.curr.join$percentile, sep=" ")
+#find unique gcm.metric.percentile to loop through each combo will have a map
+unique.gcm.metric.percentile <- unique(ffm.alt.curr.join$gcm.metric.percentile)
+#filter to only p50 for now
+ind.p50 <- grep("p50", unique.gcm.metric.percentile)
+unique.gcm.metric.percentile <- unique.gcm.metric.percentile[ind.p50]
+
+#join subbasin shapefile with ffm.alt.curr.join
+#but first set subbasin to New_Name to match basin id
+ffm.alt.curr.join <- ffm.alt.curr.join %>% 
+  rename(New_Name = subbasin)
+#join wtih basins based on New_Name
+ffm.alt.curr.join <- ffm.alt.curr.join %>% 
+  inner_join(basins, by = c('New_Name'))
+ffm.alt.curr.join
+
+
+#set colors for future categories
+color.future <- c("#4575b4", "grey", "#a50f15", "white")
+categories.future <- c("Possible Improvement", "Uncertain", "Possible Degradation", "NA")
+key <- data.frame(cbind(color.future, categories.future))
+
+#gcm lookup labels
+GCM.name <- c("CanESM2", "CNRM-CM5", "CCSM4", "MIROC5")
+GCM.labels <- c("CanESM2 (wetter/hotter)", "CNRM-CM5 (wetter/cooler)", "CCSM4 (moderate)", "MIROC5 (drier/other)")
+gcm.lookup <- data.frame(cbind(GCM.name, GCM.labels))
+
+
+for(z in 1:length(unique.gcm.metric.percentile)) {
+  #subset data to unique.gcm.metric.percentile i
+  subset <- ffm.alt.curr.join %>% 
+    filter(gcm.metric.percentile == unique.gcm.metric.percentile[z])
+  
+  #metric and gcm
+  metric.z <- unique(subset$metric)
+  gcm.z <- unique(subset$gcm)
+  #get gcm title
+  gcm.title <- gcm.lookup$GCM.labels[gcm.lookup$GCM.name == gcm.z]
+  
+  
+  #subset the color key
+  key.sub <- key[key$categories.future %in% subset$future.category,]
+  
+  #save as factor
+  key.sub$categories.future <- factor(key.sub$categories.future, levels = key.sub$categories.future)
+  subset$future.category <- factor(subset$future.category, levels = key.sub$categories.future)
+  
+  #watershed plot for future level 1
+  #filled alteration plots
+  alt.plot <- ggplot() +
+    geom_sf(data = subset, color= "#969696", aes(fill=future.category, geometry = geometry)) +
+    scale_fill_manual(name = "Future Category", labels = key.sub$categories.future, values=key.sub$color.future) +
+    geom_sf(data = reaches, color = "#67a9cf", size = 0.5) +
+    labs(title=unique(subset$title_name), subtitle=gcm.title, x ="", y = "") + 
+    #annotation_scale() +
+    #annotation_north_arrow(pad_y = unit(0.9, "cm"),  height = unit(.8, "cm"),
+                           #width = unit(.8, "cm")) +
+    theme(panel.background = element_rect(fill = "white"),
+          axis.ticks = element_blank(),
+          axis.text = element_blank(),
+          panel.grid = element_line(color = "white", size = 0.8))
+  
+  #print
+   #print(alt.plot)
+  
+  #write plot
+  #save as jpg
+  plot.fname <- paste0(dir.output.1, "/", metric.z, "_",gcm.z, "_alteration.map.jpg")
+  ggsave(alt.plot, file=plot.fname, dpi=300, height=4, width=6)
+}
+
+
+
+########################################################################
+#Level 2 - future climate effects to CSCI/ASCI metrics
+#use the same definitions as above but focus on only the most important FFMS for CSCI/ASCI
+
+#take first 3 digits of csci.asci.alt.direction
+subbasin.digits3 <- as.numeric(substr(csci.asci.alt.direction$subbasin.model, 1, 3))
+#subset to subbasins that start with 201-207 (Aliso only)
+csci.asci.alt.direction <- csci.asci.alt.direction[subbasin.digits3 >= 201 &  subbasin.digits3 <= 207, ]
+
+#join wtih basins based on New_Name
+csci.asci.alt.direction2 <- csci.asci.alt.direction %>% 
+  inner_join(basins, by = c('New_Name'))
+csci.asci.alt.direction2
+
+
+#loop through for CSCI and ASCI, create plots similar to above 
+for(i in c("CSCI", "ASCI")){
+  #subset to CSCI or ASCI
+  level2.sub <- csci.asci.alt.direction2 %>% 
+    filter(Biol == i)
+  #unique metrics level 2 sub
+  unique.metrics2 <- unique(level2.sub$metric)
+  
+  #subset percent change to only metrics in level2.sub
+  pct.change.data.sub <- pct.change.data[pct.change.data$metric %in% unique.metrics2,]
+  
+  #Combine the pct change p50 to the ffm.alteration.curr
+  pct.change.level2.join <- pct.change.data.sub %>% 
+    left_join(level2.sub, by = c("subbasin.model", "metric"))
+  
+
+  #for every subbasin.gcm.metric.percentile, if alteration direction is high and abs(pct.change < -0.1)
+  
+  pct.change.level2.join$future.category <- case_when(
+    #if alteration direction is high and percent change is less than -10%, possible improvement
+    pct.change.level2.join$Direction == "High" & pct.change.level2.join$pct.change < -0.1 ~ "Possible Improvement",
+    #if alteration direction is low and percent change is more than 10%, possible improvement
+    pct.change.level2.join$Direction == "Low" & pct.change.level2.join$pct.change > 0.1 ~ "Possible Improvement",
+    
+    #if alteration direction is high and percent change is more than 10%, possible degradation
+    pct.change.level2.join$Direction == "High" & pct.change.level2.join$pct.change > 0.1  ~ "Possible Degradation",
+    #if alteration direction is low and percent change is less than -10%, possible degradation
+    pct.change.level2.join$Direction == "Low" & pct.change.level2.join$pct.change < -0.1 ~ "Possible Degradation",
+    
+    #if alteration direction is high and percent change is less than or = 10% and >= -10%, uncertain
+    pct.change.level2.join$Direction == "High" & pct.change.level2.join$pct.change <= 0.1 & pct.change.level2.join$pct.change >= -0.1 ~ "Uncertain",
+    #if alteration direction is low and percent change is less than or = 10% and >= -10%, uncertain
+    pct.change.level2.join$Direction == "Low" & pct.change.level2.join$pct.change <= 0.1 & pct.change.level2.join$pct.change >= -0.1 ~ "Uncertain",
+    
+    
+    #if alteration direction is high and percent change is less than -10%, possible improvement
+    pct.change.level2.join$Direction == "Uncertain"   ~ "Uncertain",
+    #if alteration direction is low and percent change is more than 10%, possible improvement
+    is.na(pct.change.level2.join$Direction)  ~ "NA",
+  )
+  
+  #change future category NA to "Uncertain"
+  pct.change.level2.join$future.category[is.na(pct.change.level2.join$future.category)] <- "Uncertain"
+
+  ################
+  #Loop to make maps for each FFM and GCM, percentile % change
+  #create gcm.metric.percentile column
+  pct.change.level2.join$gcm.metric.percentile <- paste(pct.change.level2.join$gcm, pct.change.level2.join$metric, pct.change.level2.join$percentile, sep=" ")
+  #find unique gcm.metric.percentile to loop through each combo will have a map
+  unique.gcm.metric.percentile <- unique(pct.change.level2.join$gcm.metric.percentile)
+  #filter to only p50 for now
+  ind.p50 <- grep("p50", unique.gcm.metric.percentile)
+  unique.gcm.metric.percentile <- unique.gcm.metric.percentile[ind.p50]
+  
+  #set colors for future categories
+  color.future <- c("#4575b4", "grey", "#a50f15", "white")
+  categories.future <- c("Possible Improvement", "Uncertain", "Possible Degradation", "NA")
+  key <- data.frame(cbind(color.future, categories.future))
+  
+  #gcm lookup labels
+  GCM.name <- c("CanESM2", "CNRM-CM5", "CCSM4", "MIROC5")
+  GCM.labels <- c("CanESM2 (wetter/hotter)", "CNRM-CM5 (wetter/cooler)", "CCSM4 (moderate)", "MIROC5 (drier/other)")
+  gcm.lookup <- data.frame(cbind(GCM.name, GCM.labels))
+  
+  
+  for(y in 1:length(unique.gcm.metric.percentile)) {
+    #subset data to unique.gcm.metric.percentile i
+    subset2 <- pct.change.level2.join %>% 
+      filter(gcm.metric.percentile == unique.gcm.metric.percentile[y])
+
+    #metric and gcm
+    metric.z <- unique(subset2$metric)
+    gcm.z <- unique(subset2$gcm)
+    #get gcm title
+    gcm.title <- gcm.lookup$GCM.labels[gcm.lookup$GCM.name == gcm.z]
+    
+    
+    #subset the color key
+    key.sub <- key[key$categories.future %in% subset2$future.category,]
+    
+    #save as factor
+    key.sub$categories.future <- factor(key.sub$categories.future, levels = key.sub$categories.future)
+    subset2$future.category <- factor(subset2$future.category, levels = key.sub$categories.future)
+    
+    #watershed plot for future level 1
+    unique2 <- unique(subset2$future.category)
+    test <- subset2[subset2$future.category == "Possible Degradation",]
+    test2 <- subset2[subset2$future.category == "Uncertain",]
+    test.all <- data.frame(subset2)
+    
+
+    #filled alteration plots
+    alt.plot <- ggplot() +
+      #fill in the aliso subbasins that were previously unaltered (not in level2 df) as uncertain
+      geom_sf(data = subset, color= "#969696", fill="grey", aes(geometry = geometry)) +
+      geom_sf(data = subset2, color= "#969696", aes(fill=future.category, geometry = geometry)) +
+      scale_fill_manual(name = "Future Category", labels = key.sub$categories.future, values=key.sub$color.future) +
+      geom_sf(data = reaches, color = "#67a9cf", size = 0.5) +
+      labs(title=unique(subset2$metric), subtitle=gcm.title, x ="", y = "") + 
+      #annotation_scale() +
+      #annotation_north_arrow(pad_y = unit(0.9, "cm"),  height = unit(.8, "cm"),
+      #width = unit(.8, "cm")) +
+      theme(panel.background = element_rect(fill = "white"),
+            axis.ticks = element_blank(),
+            axis.text = element_blank(),
+            panel.grid = element_line(color = "white", size = 0.8))
+    
+    #print
+    print(alt.plot)
+    
+    #write plot
+    #save as jpg
+    plot.fname <- paste0(dir.output.2, "/", i,"_", metric.z, "_",gcm.z, "_level2_alteration.map.jpg")
+    ggsave(alt.plot, file=plot.fname, dpi=300, height=4, width=6)
+  }
+  
+  
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+#########OLD SCRIPT BELOW
 #load in data
 
 #component alteration data
