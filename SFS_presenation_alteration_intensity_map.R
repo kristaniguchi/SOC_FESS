a32c6fe7218cdc9e605ca1ec1e5fd8cdf8d6430c
kristaniguchi <kristinetq@sccwrp.org>
2021-07-20 15:40:37 -0700

    updated libraries

--- SFS_presenation_alteration_intensity_map.R
+++ SFS_presenation_alteration_intensity_map.R
@@ -4,29 +4,45 @@
 library("tidyverse")
 library("ggplot2")
 library("dplyr")
+library(spData)
+#install.packages("spDataLarge", repos = "https://nowosad.github.io/drat/", type = "source")
+library(spDataLarge)
+
+library(tidyverse)
+library(readxl)
+library(sf)
+library(ggsn)
+library(ggmap)
+library(mapview)
+library(spData)      
+library(spDataLarge)
+library(geosphere)
+library(rgeos)
 
 
 ######################################################################
 #Level 1 alteration data exploration
 
 #alteration directory
-alt.dir.name <- "Oso_SmallCreeks"
+#alt.dir.name <- "Oso_SmallCreeks"
+alt.dir.name <- "Aliso_Oso_SmCk_SanJuan_all_lowflowbias"
 alteration.dir <- paste0("L:/San Juan WQIP_KTQ/Data/RawData/From_Geosyntec/South_OC_Flow_Ecology_for_SCCWRP/KTQ_flowalteration_assessment/", alt.dir.name)
 
 #read in combined ffm alteration: Aliso, Oso, small creeks
-alt.data <- read.csv(paste0(alteration.dir, "/ffm_alteration.df.overall.join.Aliso.Oso.SmallCreeks.csv"))
+alt.data <- read.csv(paste0(alteration.dir, "/ffm_alteration.df.overall.join.Aliso.Oso.SmallCreeks.SanJuanLSPC.lowflowbias.csv")) %>% 
+  mutate(New_Name = as.character(subbasin))
 ffm.sites <- unique(alt.data$subbasin.model)
 #calculate alteration intensity
 #calculate the intensity of alteration for each subbasin using all FFMs: total number of likely altered metrics out of the 24 using entire POR
 intensity.df.POR.allmetrics <- alt.data %>% 
-  group_by(subbasin, alteration.status) %>% 
+  group_by(subbasin, alteration.status, New_Name) %>% 
   tally() %>% 
   ungroup()  %>% 
   filter(alteration.status == "likely_altered")
 
 #create New_Name column with subbasin id to match polygon layer
 #subbasin polygon shapefile
-basins <- st_read("data/subbasin_boundaries_forSCCWRP.shp", quiet = T)
+basins <- st_read("data/Agg_Boundaries_v14.shp", quiet = T)
 #basins <- st_read('L:/San Juan WQIP_KTQ/Data/SpatialData/Model_Subbasins_Reaches/New_Subbasins_forSCCWRP_12062019/New_Subbasins_forSCCWRP/subbasin_boundaries_forSCCWRP.shp', quiet = T)
 basins2 <- basins %>% 
   inner_join(intensity.df.POR.allmetrics, by = c('New_Name'))
@@ -47,9 +63,9 @@ intensity
 study2 <- ggplot(basins) + 
   geom_sf(color = "#969696", fill="#d9d9d9") +
   #labs(title=unique(basins4.sub$title_name),x ="", y = "") + 
-  annotation_scale() +
-  annotation_north_arrow(pad_y = unit(0.9, "cm"),  height = unit(.8, "cm"),
-                         width = unit(.8, "cm")) +
+  #annotation_scale() +
+  #annotation_north_arrow(pad_y = unit(0.9, "cm"),  height = unit(.8, "cm"),
+                         #width = unit(.8, "cm")) +
   theme(panel.background = element_rect(fill = "white"),
         axis.ticks = element_blank(),
         axis.text = element_blank(),
@@ -68,6 +84,6 @@ alt.plot
 
 #write plot
 #save as jpg
-plot.fname <- paste0(dir.alt, "Alteration_intesnity_Aliso_Oso_smallstreams_map.jpg")
+plot.fname <- paste0(alteration.dir, "/Alteration_intensity_Aliso_Oso_smallstreams_SanJuan_ALL_lowflowbias.map.jpg")
 ggsave(alt.plot, file=plot.fname, dpi=300, height=8, width=12)
 
